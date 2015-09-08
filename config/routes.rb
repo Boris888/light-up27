176eea9df85b1ace9b77f69cf855e4e102d2c5c3
Boris Hruruguen <boris.huruguen@gmail.com>
2015-09-08 14:36:20 +0200

    avancement

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,26 @@
 Rails.application.routes.draw do
+  # get 'courses/index'
+
+  # get 'courses/show'
+
+  # get 'courses/new'
+
+  # get 'courses/create'
+
+  # get 'courses/edit'
+
+  # get 'courses/update'
+
+  # get 'courses/destroy'
+
+
+
   get 'omniauth_callbacks/facebook'
 
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
 
   mount Upmin::Engine => '/admin'
-  root to: 'visitors#index'
+  root to: 'courses#index'
   # devise_for :users
   resources :users
 end
